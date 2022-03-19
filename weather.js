9975655f4c4978ed68f94b42f14f9a5301a91fc7
sukmorga <naginks@outlook.com>
2022-03-19 23:28:09 +0200

    add axios request

--- weather.js
+++ weather.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.sevice.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Токен сохранен');
     } catch (e) {
         printError(e.message);
@@ -27,7 +33,10 @@ const initCLI = () => {
         return saveToken(args.t)
         //Сохранить токен
     }
+
+    getWeather('moscow');
     //Вывести погоду    
 };
+
 
 initCLI();
